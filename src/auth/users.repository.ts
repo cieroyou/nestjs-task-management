e66da28f29ffbb2d3ec4fb860491da8d0e0e42d9
sera <cieroyou1@gmail.com>
2023-05-14 18:50:19 +0900

    bcrypt 의존성 추가, db user.password 에 해싱적용한 password 저장

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -6,6 +6,7 @@ import {
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersRespository extends Repository<User> {
@@ -15,8 +16,9 @@ export class UsersRespository extends Repository<User> {
 
   async createUser(authCredentialsDto: AuthCredentialsDto) {
     const { username, password } = authCredentialsDto;
-    console.log(`createUser ${username} ${password}`);
-    const user = this.create({ username, password });
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const user = this.create({ username, password: hashedPassword });
     try {
       await this.save(user);
     } catch (error) {
