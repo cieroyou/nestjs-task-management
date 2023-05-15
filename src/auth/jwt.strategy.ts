f2e6a876bb2c96256f6ec4043b1d0170ad4593db
sera <cieroyou1@gmail.com>
2023-05-15 20:56:47 +0900

    JwtStrategy 구현

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { UsersRespository } from './users.repository';
+import { JwtPayload } from './jwt-payload.interface';
+import { User } from './user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly usersRepository: UsersRespository) {
+    super({
+      secretOrKey: 'topSecret51',
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    });
+  }
+
+  async validate(payload: JwtPayload): Promise<User> {
+    const { username } = payload;
+    const user = await this.usersRepository.findOne({
+      where: { username: username },
+    });
+    if (!user) {
+      throw new UnauthorizedException('Please check in login credentials');
+    }
+    return user;
+  }
+}
